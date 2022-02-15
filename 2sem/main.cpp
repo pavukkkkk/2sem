fce00e6619abdfccf1d99f059692f647937f2efa
pavukkkkk <kirilenkokonstantin2003@gmail.com>
2022-02-16 00:27:24 +0400

    Initial Commit

---
+++ 2sem/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2sem
+//
+//  Created by Константин Кириленко on 16.02.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
