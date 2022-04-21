471eca75e4a5380e05923ab6cc77477485375e83
KonstantinK <89852159+pavukkkkk@users.noreply.github.com>
2022-04-21 15:14:22 +0400

    Update main.cpp

--- I_vec_list/main.cpp
+++ I_vec_list/main.cpp
@@ -8,37 +8,42 @@ int main() {
     vector <int>::iterator Iterv;
     vector<int> vec;
     list<int> lst;
-    int n,a;
-        cin>>n;
-        for (int i=0;i<n;i++){
-            cin>>a;
-            vec.push_back(a);
-            lst.push_back(a);
-          
-        }
-    if (*(lst.begin())<*(lst.end())){
-        Iterl=lst.end();
+    int n, a;
+    cin >> n;
+    if (n > 1) {
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        vec.push_back(a);
+        lst.push_back(a);
+
+    }
+    if (*(lst.begin()) < lst.back()) {
+        Iterl = lst.end();
         Iterl--;
     }
-    else{
-        Iterl=lst.begin();
+    else {
+        Iterl = lst.begin();
     }
     lst.erase(Iterl);
-    Iterv=vec.end();
+    Iterv = vec.end();
     Iterv--;
-    for (int i=0;i<2;i++)
-        vec[i]=*(Iterv)*(-1);
-    for(int i: lst){
-            
-            cout<<i<<" ";
-        
-        }
-    cout<<endl;
-    for(int i: vec){
-            
-            cout<<i<<" ";
-        
-        }
+    for (int i = 0; i < 2; i++)
+        vec[i] = (*(Iterv)) * (-1);
+    for (int i : lst) {
+
+        cout << i << " ";
+
+    }
+    cout << endl;
+    for (int i : vec) {
+
+        cout << i << " ";
+
+    }
+}
+     else {
+     cout << "Error";
+    }
     return 0;
 }
 /*
@@ -46,7 +51,7 @@ int main() {
  2 3 4 5 9
  2 3 4 5
  -9 -9 4 5 9
- 
+
  4
  9 3 7 2
  3 7 2
