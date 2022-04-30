94f75e7bce9d197b3b7439c8c1ba590d926daf7d
pavukkkkk <kirilenkokonstantin2003@gmail.com>
2022-05-01 03:32:01 +0400

    vse

--- map_I/main.cpp
+++ map_I/main.cpp
@@ -1,14 +1,64 @@
-//
-//  main.cpp
-//  map_I
-//
-//  Created by Константин Кириленко on 24.04.2022.
-//
-
+#include <map>
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <string>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+bool dig(string s) {
+    set<char> n{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+    if (s[0] == '-' && s.length() > 1 || n.find(s[0]) != n.end()) {
+        for ( int i = 1; i < s.length(); i++) {
+            if (n.find(s[i]) == n.end()) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
+
+
+int main()
+{
+    int n, i=0;
+    string s;
+    map<int, string> m1;
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+    while (in >> s) {
+        i++;
+        if (!dig(s)) {
+            m1[i] = s;
+        }
+    }
+
+    for (auto x : m1)
+    {
+        if (x.second.length() % (x.first) == 0)
+        {
+            cout << x.first << " " << x.second << endl;
+        }
+    }
+    in.close();
+    out.close();
+  
+}
+/*
+100
+-213
+q21
+-
+-1
+214
+2fd1
+*/
+
+
--- map_II/main.cpp
+++ map_II/main.cpp
@@ -1,14 +1,236 @@
-//
-//  main.cpp
-//  map_II
-//
-//  Created by Константин Кириленко on 24.04.2022.
-//
-
+#include <map>
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <set>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <sstream>
+using namespace std;
+class Catalog;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+
+class AllAboutProducer{
+    vector<string> movies;
+    vector<string> info;
+    friend Catalog;
+};
+
+
+class Catalog{
+    std::map<string,AllAboutProducer> ctlg;
+    vector<string>::iterator iter;
+    
+public:
+    void addProducer(string a){
+        ctlg[a];
+    }
+    void addMovie(string a, string b){
+        if (ctlg.find(a) !=ctlg.end()){
+            ctlg[a].movies.push_back(b);
+            
+        }
+        else{
+            cout<<"Rejissera net v spiske\n";
+        }
+        
+    }
+    void changeProducer(string a, string b){
+        if (ctlg.find(a) !=ctlg.end()){
+        AllAboutProducer tmp = ctlg[a];
+        ctlg.erase(a);
+        ctlg[b]=tmp;
+        }
+        else{
+            cout<<"Rejissera net v spiske\n";
+        }
+    }
+    void delProducer(string a){
+        if (ctlg.find(a) !=ctlg.end()){
+        ctlg.erase(a);
+            
+            }
+            else{
+                cout<<"Rejissera net v spiske\n";
+            }
+    }
+    void changeMovie(string a, string b, string c){
+        if (ctlg.find(a) !=ctlg.end()){
+        for (string &x: ctlg[a].movies){
+            if (x==b){
+                x=c;
+                return;
+            }
+        }
+        cout<<"Filma net v spiske\n";
+        }
+        else{
+            cout<<"Rejissera net v spiske\n";
+        }
+    }
+    void dellMovie(string a,string b){
+        if (ctlg.find(a) !=ctlg.end()){
+        iter =find(ctlg[a].movies.begin(), ctlg[a].movies.end(), b);
+        if (iter!=ctlg[a].movies.end()){
+            ctlg[a].movies.erase(iter);
+        }
+        else{
+            cout<<"Filma net v spiske\n";
+        }
+    }
+    else{
+        cout<<"Rejissera net v spiske\n";
+    }
+    }
+    void addInfo(string a, string b){
+        if (ctlg.find(a) !=ctlg.end()){
+            ctlg[a].info.push_back(b);
+            
+        }
+        else{
+            cout<<"Rejissera net v spiske\n";
+        }
+        
+    }
+    void show_if_word(string a) {
+        for (auto i : ctlg) {
+            for (auto j: i.second.movies){
+                if (j.find(a)<j.length()){
+                    if ((j.find(a)==0 || j[j.find(a)-1] ==' ')&& (j.find(a)+a.length()==j.length() || j[j.find(a)+a.length()] ==' ')){
+                        cout<<j<<endl;
+                    }
+                }
+            }
+        }
+    }
+    void show(string a){
+        if (ctlg.find(a) !=ctlg.end()){
+            cout<<a<<": ";
+            for (string x: ctlg[a].movies){
+                cout<<x<<"|";
+            }
+    }
+        else{
+            cout<<"Rejissera net v spiske\n";
+        }
+    }
+    void show(){
+        for (auto i : ctlg) {
+            cout<<i.first;
+            cout<<"[";
+            for (auto j: i.second.info){
+                cout<<j<<"; ";
+            }
+            cout<<"]: ";
+            for (auto j: i.second.movies){
+                cout<<j<<"; ";
+            }
+            cout<<endl;
+    }
+    }
+};
+
+int main() {
+
+    ifstream in("input.txt");
+    setlocale(LC_ALL, "Russian");
+    Catalog catalog;
+    string  s, s1,s2,s3;
+    while (getline(in, s)) {
+        if (s!="-"){
+        stringstream sio(s);
+        getline(sio, s1,'|');
+        getline(sio, s2,'|');
+        catalog.addProducer(s1);
+        catalog.addInfo(s1, s2);
+        while (getline(sio, s2,'|'))
+        {
+            catalog.addMovie(s1, s2);
+                    }
+        
+                }
+    }
+        
+    
+   
+        int n;
+
+    do {
+        catalog.show();
+        cout << "1) добавить режиссера" << endl;
+        cout << "2) удалить режиссера" << endl;
+        cout << "3) добавить фильм" << endl;
+        cout << "4) удалить фильм" << endl;
+        cout << "5) изменить навзание фильма" << endl;
+        cout << "6) изменить имя режиссера" << endl;
+        cout << "7) найти фильмы, названия которых содержат зажанное слово" << endl;
+        cout << "8) добавить информацию о режиссере" << endl;
+        cout << "9) выход" << endl;
+
+        std::cin >> n;
+        cin.get();
+        switch (n) {
+        case 1: {
+            getline(cin, s);
+            catalog.addProducer(s);
+            break;
+            }
+        case 2: {
+            getline(cin, s);
+            catalog.delProducer(s);
+            break;
+            }
+        case 3: {
+            getline(cin, s1);
+            getline(cin, s2);
+            catalog.addMovie(s1, s2);
+            break;
+            }
+        case 4: {
+            getline(cin, s1);
+            getline(cin, s2);
+            catalog.dellMovie(s1, s2);
+            break;
+            }
+        case 5: {
+            getline(cin, s1);
+            getline(cin, s2);
+            getline(cin, s3);
+            catalog.changeMovie(s1, s2,s3);
+            break;
+            }
+        case 6: {
+            getline(cin, s1);
+            getline(cin, s2);
+            catalog.changeProducer(s1, s2);
+            break;
+            }
+        case 7: {
+            getline(cin, s1);
+            catalog.show_if_word(s1);
+            break;
+            }
+        case 8: {
+            getline(cin, s1);
+            getline(cin, s2);
+            catalog.addInfo(s1, s2);
+            break;
+            }
+        
+        case 9: {
+            return 0;
+            }
+        }
+
+    } while (true);
+    }
+
+
+
